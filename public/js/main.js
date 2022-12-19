3bb3e646cac1580152b92ab858392cbe04cf4909
Yassine Ebd <elbodmoussiyassine@gmail.com>
2022-12-19 11:28:13 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let prenom;
+prenom = `Yassine`
+console.log(prenom);
+let age;
+age =23;
+console.log(age);
+console.log(`je m'apelle ${prenom} et j'ai ${age} ans`);
+alert(`je m'apelle ${prenom} et j'ai ${age} ans`);
